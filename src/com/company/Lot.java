4ad437a12a85ca2e770aa783cbf48aef9b3c6bfd
Sanjit Verma <sanjitvermasw@gmail.com>
2020-03-29 21:55:23 -0400

    LVL3 MP2 Final Edition

--- src/com/company/Lot.java
+++ src/com/company/Lot.java
@@ -3,25 +3,31 @@ import java.util.ArrayList;
 import java.io.IOException;
 import java.util.Scanner;
 import java.io.File;
-public class Lot {
-    public String lotName;
-    public ArrayList<Vehicle> vehicles;
+import java.io.FileWriter;
 
-    //Lot()
-    public Lot() {
+//Lotname String
+public class Lot{
+    String lotName;
+//Vehicle Arraylist
+    ArrayList<Vehicle> vehicles;
+    public static void main(String[] args) throws IOException {
+        Lot test = new Lot();
+        test.loadVehicles("lot.txt");
+        test.saveVehicles("lot.txt");
+    }
+//Lot()
+    public Lot(){
         vehicles = new ArrayList<>();
     }
-
-    //Getter
+//Getter
     public String getLotName() {
         return lotName;
     }
-
     public ArrayList<Vehicle> getVehicles() {
         return vehicles;
     }
 
-    //Setter
+//Setter
     public void setLotName(String lotName) {
         this.lotName = lotName;
     }
@@ -30,106 +36,168 @@ public class Lot {
         this.vehicles = vehicles;
     }
 
-    //Load Vehicle
-    public void loadVehicle(String fileName) throws IOException {
+//Load Vehicle
+    public void loadVehicles (String fileName) throws IOException {
         File fl = new File(fileName);
         Scanner sc = new Scanner(fl);
+
         while (sc.hasNext()) {
-            if (sc.nextLine() == "Car" || sc.nextLine() == "car ") {
+            String type = sc.nextLine().trim();
+            sc.skip("Year: ");
+
+            int year = Integer.parseInt(sc.nextLine().trim());
+            sc.skip("Make: ");
+
+            String make = sc.nextLine();
+            sc.skip("Model: ");
+
+            String model = sc.nextLine();
+            sc.skip("Mileage:");
+
+            int mileage = Integer.parseInt(sc.nextLine().trim());
+            sc.skip("Miles Per Gallon: ");
+
+            int mpg = Integer.parseInt(sc.nextLine());
+            sc.skip("Color: ");
+
+            String color = sc.nextLine();
+            sc.skip("Seats: ");
+
+            int seats = Integer.parseInt(sc.nextLine());
+            sc.skip("Doors: ");
+
+            int doors = Integer.parseInt(sc.nextLine());
+            sc.skip("Price: ");
+
+            double price = Double.parseDouble(sc.nextLine());
+            sc.skip("VehicleID: ");
+
+            String id = sc.nextLine();
+            sc.skip("Engine specifications");
+            String[] engineInfo = sc.nextLine().split(", ");
+
+            if (type.equals("Car")||type.equals("car")) {
+                Engine newEngine = new Engine();
+                newEngine.setTransmission(engineInfo[0]);
+                newEngine.setFourWheelDrive(engineInfo[1] == "4WD");
+                String[] cylinders = engineInfo[2].split(" ");
+                newEngine.setCylinders(Integer.parseInt(cylinders[0]));
+                newEngine.setHybrid(engineInfo[3] == "hybrid");
+                String[] hp = engineInfo[4].split("");
+                newEngine.setHorsePower(Double.parseDouble(hp[0]));
+
                 Car cars = new Car();
-                cars.setMpg(Integer.parseInt(sc.nextLine()));
-                cars.setColor(sc.nextLine());
-                Engine engines = new Engine();
-                cars.setVehicleID(sc.nextLine());
-                cars.setSeats(Integer.parseInt(sc.nextLine()));
-                cars.setDoors(Integer.parseInt(sc.nextLine()));
-                cars.setMake(sc.nextLine());
-                cars.setModel(sc.nextLine());
-                cars.setMileage(Integer.parseInt(sc.nextLine()));
-                cars.setYear(Integer.parseInt(sc.nextLine()));
-                cars.setPrice(Double.parseDouble(sc.nextLine()));
-
-                String[] engineInfo = sc.nextLine().split(",");
-                engines.setTransmission(engineInfo[0]);
-                engines.setCylinders(Integer.parseInt(engineInfo[2]));
-                engines.setFourWheelDrive(engineInfo[0] == "4WD ");
-                engines.setHybrid(engineInfo[3] == "hybrid");
-                engines.setHorsePower(Double.parseDouble(engineInfo[4]));
-                cars.setEngine(engines);
-                cars.setConvertible(sc.nextLine() == "Yes");
+                cars.setYear(year);
+                cars.setMake(make);
+                cars.setModel(model);
+                cars.setMileage(mileage);
+                cars.setMpg(mpg);
+                cars.setColor(color);
+                cars.setSeats(seats);
+                cars.setDoors(doors);
+                cars.setPrice(price);
+                cars.setVehicleID(id);
+                cars.setEngine(newEngine);
+                sc.skip("Convertible: ");
+                cars.setConvertible(sc.nextLine().trim().equals("Yes"));
                 vehicles.add(cars);
-            } else if (sc.nextLine() == "Truck" || sc.nextLine() == "truck ") {
-                Truck trucks = new Truck();
-                trucks.setMpg(Integer.parseInt(sc.nextLine()));
-                trucks.setColor(sc.nextLine());
-                Engine engines = new Engine();
-                trucks.setVehicleID(sc.nextLine());
-                trucks.setSeats(Integer.parseInt(sc.nextLine()));
-                trucks.setDoors(Integer.parseInt(sc.nextLine()));
-                trucks.setMake(sc.nextLine());
-                trucks.setModel(sc.nextLine());
-                trucks.setMileage(Integer.parseInt(sc.nextLine()));
-                trucks.setYear(Integer.parseInt(sc.nextLine()));
-                trucks.setPrice(Double.parseDouble(sc.nextLine()));
+            }
+            else if (type.equals("Truck")||type.equals("truck")){
+                Engine newEngine = new Engine();
+                newEngine.setTransmission(engineInfo[0]);
+                newEngine.setFourWheelDrive(engineInfo[1] == "4WD");
+                String[] cylinders = engineInfo[2].split(" ");
+                newEngine.setCylinders(Integer.parseInt(cylinders[0]));
+                newEngine.setHybrid(engineInfo[3] == "hybrid");
+                String[] hp = engineInfo[4].split("");
+                newEngine.setHorsePower(Double.parseDouble(hp[0]));
 
-                String[] engineInfo = sc.nextLine().split(",");
-                engines.setTransmission(engineInfo[0]);
-                engines.setCylinders(Integer.parseInt(engineInfo[2]));
-                engines.setFourWheelDrive(engineInfo[0] == "4WD ");
-                engines.setHybrid(engineInfo[3] == "hybrid");
-                engines.setHorsePower(Double.parseDouble(engineInfo[4]));
-                trucks.setEngine(engines);
+                Truck trucks = new Truck();
+                trucks.setYear(year);
+                trucks.setMake(make);
+                trucks.setModel(model);
+                trucks.setMileage(mileage);
+                trucks.setMpg(mpg);
+                trucks.setColor(color);
+                trucks.setSeats(seats);
+                trucks.setDoors(doors);
+                trucks.setPrice(price);
+                trucks.setVehicleID(id);
+                trucks.setEngine(newEngine);
+                sc.skip("Bed Space: ");
                 trucks.setBedSpace(Double.parseDouble(sc.nextLine()));
                 vehicles.add(trucks);
-            } else if (sc.nextLine() == "Suv" || sc.nextLine() == "suv ") {
+
+            }
+            else if (type.equals("SUV")||type.equals("suv")) {
+                Engine newEngine = new Engine();
+                newEngine.setTransmission(engineInfo[0]);
+                newEngine.setFourWheelDrive(engineInfo[1] == "4WD");
+                String[] cylinders = engineInfo[2].split(" ");
+                newEngine.setCylinders(Integer.parseInt(cylinders[0]));
+                newEngine.setHybrid(engineInfo[3] == "hybrid");
+                String[] hp = engineInfo[4].split("");
+                newEngine.setHorsePower(Double.parseDouble(hp[0]));
+
                 SUV suvs = new SUV();
-                suvs.setMpg(Integer.parseInt(sc.nextLine()));
-                suvs.setColor(sc.nextLine());
-                Engine engines = new Engine();
-                suvs.setVehicleID(sc.nextLine());
-                suvs.setSeats(Integer.parseInt(sc.nextLine()));
-                suvs.setDoors(Integer.parseInt(sc.nextLine()));
-                suvs.setMake(sc.nextLine());
-                suvs.setModel(sc.nextLine());
-                suvs.setMileage(Integer.parseInt(sc.nextLine()));
-                suvs.setYear(Integer.parseInt(sc.nextLine()));
-                suvs.setPrice(Double.parseDouble(sc.nextLine()));
-
-                String[] engineInfo = sc.nextLine().split(",");
-                engines.setTransmission(engineInfo[0]);
-                engines.setCylinders(Integer.parseInt(engineInfo[2]));
-                engines.setFourWheelDrive(engineInfo[0] == "4WD ");
-                engines.setHybrid(engineInfo[3] == "hybrid");
-                engines.setHorsePower(Double.parseDouble(engineInfo[4]));
-                suvs.setEngine(engines);
+                suvs.setYear(year);
+                suvs.setMake(make);
+                suvs.setModel(model);
+                suvs.setMileage(mileage);
+                suvs.setMpg(mpg);
+                suvs.setColor(color);
+                suvs.setSeats(seats);
+                suvs.setDoors(doors);
+                suvs.setPrice(price);
+                suvs.setVehicleID(id);
+                suvs.setEngine(newEngine);
+                sc.skip("Cargo Space:  ");
                 suvs.setCargoSpace(Double.parseDouble(sc.nextLine()));
-                vehicles.add(suvs);
-            } else if (sc.nextLine() == "Minivan" || sc.nextLine() == "minivan ") {
-                Minivan minivans = new Minivan();
-                minivans.setMpg(Integer.parseInt(sc.nextLine()));
-                minivans.setColor(sc.nextLine());
-                Engine engines = new Engine();
-                minivans.setVehicleID(sc.nextLine());
-                minivans.setSeats(Integer.parseInt(sc.nextLine()));
-                minivans.setDoors(Integer.parseInt(sc.nextLine()));
-                minivans.setMake(sc.nextLine());
-                minivans.setModel(sc.nextLine());
-                minivans.setMileage(Integer.parseInt(sc.nextLine()));
-                minivans.setYear(Integer.parseInt(sc.nextLine()));
-                minivans.setPrice(Double.parseDouble(sc.nextLine()));
 
-                String[] engineInfo = sc.nextLine().split(",");
-                engines.setTransmission(engineInfo[0]);
-                engines.setCylinders(Integer.parseInt(engineInfo[2]));
-                engines.setFourWheelDrive(engineInfo[0] == "4WD ");
-                engines.setHybrid(engineInfo[3] == "hybrid");
-                engines.setHorsePower(Double.parseDouble(engineInfo[4]));
-                minivans.setEngine(engines);
-                minivans.setSlidingDoors(sc.nextLine() == "Yes");
+                sc.skip("Removable Seats:");
+                suvs.setRemovableSeats(sc.nextLine().trim().equals("Yes"));
+                vehicles.add(suvs);
+            }
+
+            else if (type.equals("Van") ||type.equals("van")) {
+                Engine newEngine = new Engine();
+                newEngine.setTransmission(engineInfo[0]);
+                newEngine.setFourWheelDrive(engineInfo[1] == "4WD");
+                String[] cylinders = engineInfo[2].split(" ");
+                newEngine.setCylinders(Integer.parseInt(cylinders[0]));
+                newEngine.setHybrid(engineInfo[3] == "hybrid");
+                String[] hp = engineInfo[4].split("");
+                newEngine.setHorsePower(Double.parseDouble(hp[0]));
+
+                Minivan minivans = new Minivan();
+                minivans.setYear(year);
+                minivans.setMake(make);
+                minivans.setModel(model);
+                minivans.setMileage(mileage);
+                minivans.setMpg(mpg);
+                minivans.setColor(color);
+                minivans.setSeats(seats);
+                minivans.setDoors(doors);
+                minivans.setPrice(price);
+                minivans.setVehicleID(id);
+                minivans.setEngine(newEngine);
+                sc.skip("Sliding doors: ");
+                minivans.setSlidingDoors(sc.nextLine().equals("Yes"));
                 vehicles.add(minivans);
             }
         }
+        for (Vehicle printInfo : vehicles) {
+            System.out.println(printInfo.toString());
+        }
     }
-    public static void main(String args[]) throws IOException {
+ //Save Vehicle
+    public void saveVehicles (String fileName) throws IOException {
+        FileWriter fileWriter = new FileWriter(fileName);
+        for(Vehicle v:vehicles) {
+            String save="";
+            save += v.toString();
+            fileWriter.write(save);
+        }
+        fileWriter.close();
     }
 }
--- src/com/company/SUV.java
+++ src/com/company/SUV.java
@@ -5,22 +5,29 @@ package com.company;
 
 public class SUV extends Vehicle {
     public double cargoSpace;
-
+    public boolean removableSeats;
 //SUV()
     public SUV() { }
 
 //SUV + All properties of SUV
-    public SUV(int mpg , String color  , Engine engine , String vehicleID , int seats , int doors , String make , String model , int mileage , int year , double price , double cargoSpace ){
+    public SUV(int mpg , String color  , Engine engine , String vehicleID , int seats , int doors , String make , String model , int mileage , int year , double price , double cargoSpace, boolean removableSeats ){
         super (mpg , color , engine , vehicleID , seats , doors , make , model , mileage , year , price);
         this.cargoSpace= cargoSpace;
+        this.removableSeats= removableSeats;
     }
 //Getter
     public double getCargoSpace() {
         return cargoSpace;
     }
+    public boolean isRemovableSeats () {
+        return removableSeats;
+    }
 //Setter
     public void setCargoSpace(double cargoSpace) {
         this.cargoSpace = cargoSpace;
+    }
+    public void setRemovableSeats ( boolean removableSeats){
+        this.removableSeats = removableSeats;
     }
 //to string methods
     public String toString() {
